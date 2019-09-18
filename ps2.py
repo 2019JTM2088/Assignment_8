054d4fe8be165f7f2d7b3be9f53e430103dd70dd
2019JTM2088 <jtm192088@iitd.ac.in>
2019-09-18 11:58:44 +0530

    second program improvement

--- ps2.py
+++ ps2.py
@@ -1,1 +1,17 @@
+#! /usr/bin/python3
 
+# importing all necessary libraries 
+import random 
+from time import sleep 
+
+print("Welcome to the Game!")
+
+# Creates an empty board 
+def create_board(): 
+    return(np.array([[0, 0, 0], 
+                     [0, 0, 0], 
+                     [0, 0, 0]])) 
+                     
+                     
+ # Lets the player type which letter they want to be.                     
+  
