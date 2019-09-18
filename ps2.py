51ca281938bbb19775b4a1b544cbe6926b06d157
2019JTM2088 <jtm192088@iitd.ac.in>
2019-09-18 14:52:25 +0530

    Second program board some improvement

--- ps2.py
+++ ps2.py
@@ -18,5 +18,25 @@ def draw():
     print (board)
 
 draw()
+List1=[1,3,5,7,9]
+List2=[2,4,6,8]
 
+# Check for empty places on board 
+def possibilities(board): 
+    l = [] 
+      
+    for i in range(len(board)): 
+        for j in range(len(board)): 
+              
+            if board[i][j] == 0: 
+                l.append((i, j)) 
+    return(l) 
+    
+while not winner :
+    draw()
+
+    if playerOneTurn :
+        print( "Player 1:")
+    else :
+        print( "Player 2:")
 
